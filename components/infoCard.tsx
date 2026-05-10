518d09fee29820fb868c5bc81893d904c72299a3
Charandeep <sangaticherry3306@gmail.com>
2026-05-10 14:12:40 +0530

    fix: make icon prop optional in InfoCard to fix build

--- components/infoCard.tsx
+++ components/infoCard.tsx
@@ -7,13 +7,15 @@ export const InfoCard = ({
 }: {
   title: string;
   value: string;
-  icon: LucideIcon;
+  icon?: LucideIcon;
 }) => {
   return (
     <div className="flex gap-5 items-center overflow-hidden">
-      <div className="bg-gray-900 p-4 rounded-xl flex items-center justify-center shrink-0 border border-gray-800">
-        <Icon className="w-4 h-4 text-blue-500" />
-      </div>
+      {Icon && (
+        <div className="bg-gray-900 p-4 rounded-xl flex items-center justify-center shrink-0 border border-gray-800">
+          <Icon className="w-4 h-4 text-blue-500" />
+        </div>
+      )}
 
       <div className="flex flex-col gap-1">
         <h2 className="text-white/60 text-xs font-bold uppercase">{title}</h2>
